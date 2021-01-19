b8e82d74e82d1d92c359d42d96b223837127e84c
Nicolas Horde <enhorde@gmail.com>
2021-01-19 10:33:24 -0500

    New branch

--- main.py
+++ main.py
@@ -6,29 +6,29 @@ from labyrintheLogique import *
 from labyrintheInterfaceUtilisateur import *
 
 def main():
-	print("              Résolution d'un labyrinthe par le plus court chemin             ")
+	print("              RÃ©solution d'un labyrinthe par le plus court chemin             ")
 	print()
 	print()
 	t=tailleLabyrinthe()
 	l=densiteLabyrinthe()
 	
-	batiment = generationBatiment(t,l) # création du labyrinthe
+	batiment = generationBatiment(t,l) # crÃ©ation du labyrinthe
 	
 	affichageBatiment(batiment,t) # affichage du labyrinthe
 	
 	a,b,c,d = programmeUtilisateur(batiment) 
 	
-# fonction de déboggage
+# fonction de dÃ©boggage
 #	affichageBatimentAmeliore(batiment,a,b,c,d,t)
 	
-	algorithmeTarjan(t,batiment,a,b,c,d) # algorithme de Tarjan (résolution du labyrinthe)
+	algorithmeTarjan(t,batiment,a,b,c,d) # algorithme de Tarjan (rÃ©solution du labyrinthe)
 	
 	k=algorithmeTarjan(t,batiment,a,b,c,d)
 	
-#fonction de déboggage
+#fonction de dÃ©boggage
 #	affichageTarjan(batiment,k,t)
 	
-	solutionLabyrinthe(batiment,a,b,c,d,t) # on vérifie si le labyrinthe a une solution
+	solutionLabyrinthe(batiment,a,b,c,d,t) # on vÃ©rifie si le labyrinthe a une solution
 	h=solutionLabyrinthe(batiment,a,b,c,d,t)
 	
 	algorithmeTarjanRetour(batiment,a,b,c,d,t,k,h) # calcul du retour s'il y a une solution
